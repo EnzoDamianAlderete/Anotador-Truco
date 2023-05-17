900aae494036cf1b486291055799f395d1dde1a3
EnzoDamianAlderete <enzoalderete04@gmail.com>
2023-05-17 11:52:43 -0300

    se agrega funcionalidad inicial de botones - y +

--- containers/ButtonsContainer.tsx
+++ containers/ButtonsContainer.tsx
@@ -1,12 +1,30 @@
+'use client';
+
 import { ButtonAdd } from "@/components/ButtonAdd";
 import { ButtonRemove } from "@/components/ButtonRemonve";
 
 export function ButtonsContainer(){
+
+    const Add =()=>{
+        console.log("agregando");
+    }
+
+    const Remove =()=>{
+        console.log("remove");
+    }
     
     return(
         <div className="flex flex-col gap-2">
-            <ButtonAdd/>
-            <ButtonRemove/>
+            <button 
+            onClick={()=>Add()}
+            className="p-2 rounded-full bg-blue-500"
+            >+
+            </button>
+            <button 
+            onClick={()=>Remove()}
+            className="p-2 rounded-full bg-red-500">
+            -
+            </button>
         </div>
     )
 }
