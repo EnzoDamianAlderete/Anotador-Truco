be73d946a52270fa22ff6e19a8ef81aa84233d4e
EnzoDamianAlderete <enzoalderete04@gmail.com>
2023-06-01 12:57:52 -0300

    se vuelve a layout sin cambios

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,64 +1,24 @@
 import { AnotadorProvider } from '@/context/AppContext'
 import './globals.css'
 import { Corben } from '@next/font/google'
-import { MyContextProps } from "../context/AppContext"
 
-const corben = Corben({ subsets: ['latin'], weight: ['400'] })
+const corben = Corben({ subsets: ['latin'], weight:['400'] })
 
 export const metadata = {
   title: 'Anotador Truco',
-  description: 'Development by: Enzo Alderete',
+  description: 'Delopment by: Enzo Alderete',
 }
 
-interface RootLayoutProps extends MyContextProps {
-  children: React.ReactNode
-}
-
-export function RootLayout({
+export default function RootLayout({
   children,
-  Win,
-  setWin,
-  winner,
-  setWinner,
-  pointsEllos,
-  setPointsEllos,
-  pointsNos,
-  setPointsNos,
-  RestartGame,
-  esMayorNos,
-  setEsMayorNos,
-  esMayorEllos,
-  setEsMayorEllos,
-  isOpen,
-  setIsOpen,
-  openModal,
-  closeModal,
-}: RootLayoutProps) {
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="en">
-      <AnotadorProvider
-        Win={Win}
-        setWin={setWin}
-        winner={winner}
-        setWinner={setWinner}
-        pointsEllos={pointsEllos}
-        setPointsEllos={setPointsEllos}
-        pointsNos={pointsNos}
-        setPointsNos={setPointsNos}
-        RestartGame={RestartGame}
-        esMayorNos={esMayorNos}
-        setEsMayorNos={setEsMayorNos}
-        esMayorEllos={esMayorEllos}
-        setEsMayorEllos={setEsMayorEllos}
-        isOpen={isOpen}
-        setIsOpen={setIsOpen}
-        openModal={openModal}
-        closeModal={closeModal}
-      >
-        <body className={corben.className}>{children}</body>
+      <AnotadorProvider>
+      <body className={corben.className}>{children}</body>
       </AnotadorProvider>
     </html>
   )
 }
-
-export default RootLayout;
