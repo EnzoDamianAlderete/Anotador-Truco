e0bd2f4f9e730ab4911cdfc61d6264f8eead3c73
EnzoDamianAlderete <enzoalderete04@gmail.com>
2023-06-01 11:03:27 -0300

    se cambian botones

--- containers/ButtonsContainer.tsx
+++ containers/ButtonsContainer.tsx
@@ -13,13 +13,18 @@ export function ButtonsContainer({Add,Remove}:ButtonsContainerProps){
         <div className="flex flex-col gap-8 ">
             <button 
             onClick={Add}
-            className="p-3 rounded-full border-2"
-            >+
+            ><svg width="46" height="46" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path d="M12 2a10 10 0 1 0 0 20 10 10 0 1 0 0-20z"></path>
+            <path d="M12 8v8"></path>
+            <path d="M8 12h8"></path>
+          </svg>
             </button>
             <button 
-            onClick={Remove}
-            className="p-3 rounded-full border-2">
-            -
+            onClick={Remove}>
+            <svg width="46" height="46" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path d="M8 12h8"></path>
+            <path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10Z"></path>
+            </svg>
             </button>
         </div>
     )
