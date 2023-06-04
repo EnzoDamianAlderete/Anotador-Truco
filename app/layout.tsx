89758a3ebaefb9e69aaf1a1a43a301f3920940ab
EnzoDamianAlderete <enzoalderete04@gmail.com>
2023-06-04 11:48:54 -0300

    se corrige AnotadorProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,22 @@
-import { AnotadorProvider } from '@/context/AppContext'
-import './globals.css'
-import { Corben } from '@next/font/google'
+import { AnotadorProvider, MyContextProps } from '@/context/AppContext';
+import './globals.css';
+import { Corben } from '@next/font/google';
 
-const corben = Corben({ subsets: ['latin'], weight:['400'] })
+const corben = Corben({ subsets: ['latin'], weight: ['400'] });
 
 export const metadata = {
   title: 'Anotador Truco',
-  description: 'Delopment by: Enzo Alderete',
-}
+  description: 'Development by: Enzo Alderete',
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      
       <body className={corben.className}>
         <AnotadorProvider>
           {children}
         </AnotadorProvider>
-        </body>
-      
+      </body>
     </html>
-  )
+  );
 }
--- context/AppContext.tsx
+++ context/AppContext.tsx
@@ -1,18 +1,35 @@
-"use client";
-import React, { createContext, useState } from 'react';
-import {MyContextProps} from '../Interfaces'
+'use client'
+import React, { ReactNode, createContext, useState, Dispatch, SetStateAction } from 'react';
 
+export interface MyContextProps {
+  Win: string;
+  setWin: Dispatch<SetStateAction<string>>;
+  winner: boolean;
+  setWinner: Dispatch<SetStateAction<boolean>>;
+  isOpen: boolean;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
+  openModal: () => void;
+  closeModal: () => void;
+  pointsEllos: number;
+  esMayorEllos: boolean;
+  setEsMayorEllos: Dispatch<SetStateAction<boolean>>;
+  esMayorNos: boolean;
+  setEsMayorNos: Dispatch<SetStateAction<boolean>>;
+  setPointsEllos: Dispatch<SetStateAction<number>>;
+  pointsNos: number;
+  setPointsNos: Dispatch<SetStateAction<number>>;
+  RestartGame: () => void;
+}
 
+export const AnotadorContext = createContext<MyContextProps | undefined>(undefined);
 
-export const AnotadorContext = createContext<MyContextProps>({} as MyContextProps);
-
-export const AnotadorProvider = ({ children }:MyContextProps) => {
+export const AnotadorProvider = ({ children }: { children: ReactNode }) => {
   const [Win, setWin] = useState<string>("");
-  const [winner , setWinner] = useState<boolean>(false);
-  const [ pointsEllos , setPointsEllos] = useState<number>(0);
-  const [ pointsNos , setPointsNos] = useState<number>(0);
-  const [esMayorNos , setEsMayorNos] = useState<boolean>(false);
-  const [esMayorEllos , setEsMayorEllos] = useState<boolean>(false);
+  const [winner, setWinner] = useState<boolean>(false);
+  const [pointsEllos, setPointsEllos] = useState<number>(0);
+  const [pointsNos, setPointsNos] = useState<number>(0);
+  const [esMayorNos, setEsMayorNos] = useState<boolean>(false);
+  const [esMayorEllos, setEsMayorEllos] = useState<boolean>(false);
   const [isOpen, setIsOpen] = useState(false);
 
   const openModal = () => {
@@ -23,18 +40,35 @@ export const AnotadorProvider = ({ children }:MyContextProps) => {
     setIsOpen(false);
   };
 
-  const RestartGame =():void=>{
+  const RestartGame = (): void => {
     setPointsEllos(0);
     setPointsNos(0);
     setEsMayorNos(false);
     setEsMayorEllos(false);
     setWinner(false);
     closeModal();
-  }
+  };
 
   return (
-    <AnotadorContext.Provider
-     value={{pointsEllos,setPointsEllos,pointsNos,setPointsNos,RestartGame,children,esMayorNos,setEsMayorNos,esMayorEllos,setEsMayorEllos,isOpen, setIsOpen,openModal,closeModal,winner , setWinner,Win, setWin }}>
+    <AnotadorContext.Provider value={{
+      Win,
+      setWin,
+      winner,
+      setWinner,
+      isOpen,
+      setIsOpen,
+      openModal,
+      closeModal,
+      pointsEllos,
+      esMayorEllos,
+      setEsMayorEllos,
+      esMayorNos,
+      setEsMayorNos,
+      setPointsEllos,
+      pointsNos,
+      setPointsNos,
+      RestartGame,
+    }}>
       {children}
     </AnotadorContext.Provider>
   );
